2df63288efd3ff8581857fe47b9847a84a29615e
Sardul <thesardul@users.noreply.github.com>
2020-06-15 14:49:40 +0530

    simple DAO operations

---
+++ src/main/java/com/sardul/jdbc/helpers/JDBCConstants.java
@@ -0,0 +1,8 @@
+package com.sardul.jdbc.helpers;
+
+public class JDBCConstants {
+    public static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+    public static final String URL         = "jdbc:mysql://localhost/demo";
+    public static final String USERNAME    = "root";
+    public static final String PASSWORD    = "admin";
+}
